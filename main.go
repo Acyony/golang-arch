33ac75b44e48bab329aec2dec758494d0733a1a3
Alcione <alcionefribeiro@gmail.com>
2022-05-27 22:05:53 +0200

    unmarshal - decoding JSON into Structs

--- main.go
+++ main.go
@@ -10,6 +10,11 @@ type person struct {
 	First string
 }
 
+type Bird struct {
+	Species     string
+	Description string
+}
+
 func main() {
 	p1 := person{
 		First: "Alcione",
@@ -20,9 +25,32 @@ func main() {
 	}
 
 	xp := []person{p1, p2}
+	// NOTE: bs - byte of slice
+
+	// json.Marshal: encoding Go struct into a json string
+
 	bs, err := json.Marshal(xp)
 	if err != nil {
 		log.Panic(err)
 	}
-	fmt.Println(string(bs))
+	fmt.Println("PRINT JSON", string(bs))
+
+	xp2 := []person{}
+
+	// json.Unmarschal: decode the data from json to a struct
+	//&xp2 is the adress of the variable I want to store the parsed data in
+	err = json.Unmarshal(bs, &xp2)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println("Back into a Go data structure", xp2)
+
+	/// ---------------------- =^.^= ----------------------
+
+	birdJson := `{"species": "pigeon", "description": "likes cookies"}`
+	fmt.Println(birdJson)
+	var bird Bird
+	json.Unmarshal([]byte(birdJson), &bird)
+	fmt.Printf("Species: %s, Description: %s", bird.Species, bird.Description)
 }
